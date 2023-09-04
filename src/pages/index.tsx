4f454d0e8d89ba8b07a117c5a16846d54fa4bebd
bettermorebetter <mingyukim1827@gmail.com>
2023-09-04 13:20:59 +0900

    #3 setup complete

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-black-500">it works</h1>
+    </div>
+  );
+}
